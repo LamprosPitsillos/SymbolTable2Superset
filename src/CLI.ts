febd380ad0e494dbbd5fb57f5a1c85a770436e11
Lampros Pitsillos <hauahx@gmail.com>
2024-01-04 01:00:10 +0200

    added modular Cli command set + 2 more smells

--- src/CLI.ts
+++ src/CLI.ts
@@ -1,31 +1,49 @@
 import { ParseArgsConfig, parseArgs } from 'util';
 import { exit } from 'process';
-import { isatty } from 'tty';
+import { log } from 'console';
 
-const parse_config: ParseArgsConfig = {
+type OptionType = {
+    type: string;
+    short: string;
+    desc: string;
+    default?: boolean | string;
+};
+
+type ParseArgs = {
+    options: {
+        [key: string]: OptionType;
+    };
+};
+
+const parse_config: ParseArgs = {
     options: {
         'verbose': {
             type: 'boolean',
             short: 'v',
+            desc: "Show the data that is being pushed to database.",
             default: false,
         },
         'update-rules': {
             type: 'boolean',
             short: 'u',
+            desc: "Update the rules according to the Smells.json file provided in root of path.",
             default: false,
         },
         'seed': {
             type: 'string',
+            desc: "The Json file where the C++ SymbolTree has been stored.",
             short: 's',
         },
         'dry-run': {
             type: 'boolean',
             short: 'd',
+            desc: "No data will be send to the database thus no changes will happen ( Used primarily for testing ).",
             default: false
         },
         'help': {
             type: 'boolean',
             short: 'h',
+            desc: "See this help message.",
             default: false
         }
     }
@@ -56,7 +74,7 @@ export enum AnsiColors {
 
 
 // ANSI escape code for formatting text
-const formatText = (text: string, formatCode: string): string => isColorSupported() ? `\x1b[${formatCode}m${text}\x1b[0m` : text ;
+const formatText = (text: string, formatCode: string): string => isColorSupported() ? `\x1b[${formatCode}m${text}\x1b[0m` : text;
 
 // Functions for different text formats
 export const textColor = (text: string, color: AnsiColors): string => formatText(text, `38;5;${color}`);
@@ -79,28 +97,46 @@ function help() {
 ${bold("Help")}:
 
 ${bold("Arguments")}:
+`)
+    const terminalWidth = process.stdout.columns || 80; // Get terminal width or default to 80 columns
+    // const terminalWidth = 40; // Get terminal width or default to 80 columns
+    let helpMessage = '';
+    for (const optionName in parse_config.options) {
+        const option = parse_config.options[optionName];
+        const cmd = `${bold(`--${option.short}/-${optionName}`)}`;
+        const desc = `${option.desc ? `${option.desc}` : ''}`;
 
-${bold("--help/-h")}
-    See this help message.
+        const type = `${italic(`{ type: ${option.type}${option.default !== undefined ? `, default: ${option.default}` : ''} }`)}`;
 
-${bold("--seed/-s")}:                                                                   ${italic("{ type: string }")}
-    The Json file where the C++ SymbolTree has been stored.
+        const line = `${cmd}: ${' '.repeat(terminalWidth - cmd.length - type.length - 2)}${type}\n  ${desc}\n\n`;
 
-${bold("--update-rules/-u")}:                                          ${italic("{ type: boolean, default: false }")}
-    Update the rules according to the Smells.json file provided in root of path.
+        helpMessage += line;
+    }
+    console.log(helpMessage)
+    //
+    // `
+    // ${bold("--help/-h")}
+    //     See this help message.
+    //
+    // ${bold("--seed/-s")}:                                                                   ${italic("{ type: string }")}
+    //     The Json file where the C++ SymbolTree has been stored.
+    //
+    // ${bold("--update-rules/-u")}:                                          ${italic("{ type: boolean, default: false }")}
+    //     Update the rules according to the Smells.json file provided in root of path.
+    //
+    // ${bold("--verbose/-v")}:                                                  ${italic("{ type: boolean, default: false }")}
+    //     Output everything as it is sent to the DB.
+    //
+    // ${bold("--dry-run/-d")}:                                                  ${italic("{ type: boolean, default: false }")}
+    //     No data will be send to the database thus no changes will happen
+    //         ( Used primarily for testing ).
+    // `);
 
-${bold("--verbose/-v")}:                                                  ${italic("{ type: boolean, default: false }")}
-    Output everything as it is sent to the DB.
-
-${bold("--dry-run/-d")}:                                                  ${italic("{ type: boolean, default: false }")}
-    No data will be send to the database thus no changes will happen
-        ( Used primarily for testing ).
-`);
 }
 
 function assertArgs() {
     try {
-        const { values } = parseArgs(parse_config);
+        const { values } = parseArgs(parse_config as ParseArgsConfig);
 
         if (values.help) {
             help()
